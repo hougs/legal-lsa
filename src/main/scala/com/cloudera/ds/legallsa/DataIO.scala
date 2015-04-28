c04c7291ac9b3df9195bf6a7870bf35f63b54840
Juliet Hougland <juliet@cloudera.com>
2015-04-27 19:03:57 -0700

    Creates RowMatrix from csvs.

--- src/main/scala/com/cloudera/ds/legallsa/DataIO.scala
+++ src/main/scala/com/cloudera/ds/legallsa/DataIO.scala
@@ -4,28 +4,26 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 
 import org.apache.spark.SparkContext
-import org.apache.spark.mllib.linalg.{Matrix, Vector}
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+import org.apache.spark.mllib.linalg.{Vectors, Matrix, Vector}
 import org.apache.spark.rdd.RDD
 
 object DataIO {
-
-
   val raw_count_path = "hdfs:///user/juliet/legallsa/Word_stats_MACs_0708.csv"
   val tfidf_path = "hdfs:///user/juliet/legallsa/Word_stats_MACs_0708_TF-IDF.csv"
 
-
-  def transformLine(line: Array[String]) = {
-
-  }
-
-  def read_marix_input(sc: SparkContext, path: String): Matrix = {
+  def read_marix_input(sc: SparkContext, path: String): RowMatrix = {
     val lines: RDD[String] = sc.textFile(path)
     val header = lines.first()
-    lines.map{ line =>
+    val vectors = lines.map{ line =>
       val arr =line.split(",")
       val word_arr = arr.slice(12, arr.length)
-      word_arr.map()
+
+      val nonzeroElems: Seq[(Int, Double)] = word_arr.map(elem => elem.toDouble).zipWithIndex.filter(tuple =>
+      tuple._1 != 0.0).map(tuple => (tuple._2, tuple._1)).toSeq
+      Vectors.sparse(word_arr.length, nonzeroElems)
     }
+    new RowMatrix(vectors)
   }
 
 
