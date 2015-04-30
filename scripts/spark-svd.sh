8005284c7ccca3ee184bdcf890c2672821ea0cb2
Juliet Hougland <juliet@cloudera.com>
2015-04-30 16:05:06 -0700

    fixes to startup scripts

--- scripts/spark-svd.sh
+++ scripts/spark-svd.sh
@@ -17,21 +17,22 @@ TFIDF_CT_PATH=hdfs:///user/juliet/legallsa/out/tfidf-ct.txt
 MASTER=$1
 RANK=40
 
-export SPARK_HOME=$2
+export SPARK_HOME=/home/juliet/src/legal-lsa/spark-1.3.0-bin-hadoop2.4
 export HADOOP_CONF_DIR=/etc/hadoop/conf
 
 $SPARK_HOME/bin/spark-submit --class com.cloudera.ds.svdbench.SparkSVD \
   --conf spark.yarn.jar=hdfs:///user/juliet/bin/spark-1.3.0-bin-hadoop2.4/lib/spark-assembly-1.3.0-hadoop2.4.0.jar \
   --master $MASTER --executor-memory 14g --executor-cores 5 --num-executors 18 \
-  --driver-class-path ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-  ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  --driver-class-path ./legal-lsa-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  ./legal-lsa-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
   --matrixPath $COUNT_PATH --headerPath $COUNT_HEADER_PATH --outUPath $COUNT_U --outSPath $COUNT_S
   --outVPath $COUNT_V --conceptTermPath $COUNT_CT_PATH --rank $RANK
 
-  $SPARK_HOME/bin/spark-submit --class com.cloudera.ds.svdbench.SparkSVD \
-    --conf spark.yarn.jar=hdfs:///user/juliet/bin/spark-1.3.0-bin-hadoop2.4/lib/spark-assembly-1.3.0-hadoop2.4.0.jar \
-    --master $MASTER --executor-memory 14g --executor-cores 5 --num-executors 18 \
-    --driver-class-path ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-    ./target/svd-benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-    --matrixPath $TFIDF_PATH --headerPath $TFIDF_HEADER_PATH --outUPath $TFIDF_U --outSPath $TFIDF_S
-    --outVPath $TFIDF_V --conceptTermPath $TFIDF_CT_PATH --rank $RANK
+$SPARK_HOME/bin/spark-submit --class com.cloudera.ds.svdbench.SparkSVD \
+  --conf spark.yarn.jar=hdfs:///user/juliet/bin/spark-1.3.0-bin-hadoop2.4/lib/spark-assembly-1.3.0-hadoop2.4.0.jar \
+  --master $MASTER --executor-memory 14g --executor-cores 5 --num-executors 18 \
+  --driver-class-path ./legal-lsa-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  ./legal-lsa-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  --matrixPath $TFIDF_PATH --headerPath $TFIDF_HEADER_PATH --outUPath $TFIDF_U --outSPath $TFIDF_S
+  --outVPath $TFIDF_V --conceptTermPath $TFIDF_CT_PATH --rank $RANK
+
